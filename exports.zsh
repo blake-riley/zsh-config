505d2661502f1e4c9d20290d8d6e6c1aa668a8fb
Blake Riley <blake.riley@gmail.com>
2017-01-06 01:44:52 +1100

    (m) Fix quoting style

--- exports.zsh
+++ exports.zsh
@@ -1,31 +1,31 @@
 #!/usr/bin/env zsh
 
 #--- zsh
-export ZSH=$HOME/.oh-my-zsh
+export ZSH="${HOME}/.oh-my-zsh"
 #--- brew
 export HOMEBREW_BUILD_FROM_SOURCE=1
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 #--- Android
-export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_HOME="/usr/local/opt/android-sdk"
 #--- Amber
-export AMBERHOME=/sw/amber14
-export PATH=$AMBERHOME/bin:$PATH
+export AMBERHOME="/sw/amber14"
+export PATH="${AMBERHOME}/bin:${PATH}"
 
 #--- VMD
 export VMDDIR="/Applications/VMD-1.9.2.app/Contents/vmd"
 
 #--- Powerline
-export PY_PKGS=/usr/local/lib/python3.6/site-packages
+export PY_PKGS="/usr/local/lib/python3.6/site-packages"
 
 #--- Homebrew/Cask
 export HOMEBREW_CASK_OPTS='--appdir=/Applications'
 
 #-------- PATH -------- -----------------------#
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/local:$PATH
-export PATH=$HOME/bin:$PATH
-export PATH=$HOME/.local/bin:$PATH
-export PATH=$HOME/.cabal/bin:$PATH
-export PATH=$PATH:`dirname '/Applications/APBS.app/Contents/MacOS/apbs_term'`
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local:${PATH}"
+export PATH="${HOME}/bin:${PATH}"
+export PATH="${HOME}/.local/bin:${PATH}"
+export PATH="${HOME}/.cabal/bin:${PATH}"
+export PATH="${PATH}:$(dirname '/Applications/APBS.app/Contents/MacOS/apbs_term')"
 
 
 #------ COMPILING ----- -----------------------#
