f68bd6e9f357665eed29c5db8307551ae2998211
Blake Riley <blake.riley@gmail.com>
2020-02-10 00:16:26 -0500

    Add p10k customisation

--- .zshrc
+++ .zshrc
@@ -10,6 +10,14 @@
 ###############################################
 ###############################################
 
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block, everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+
 #----- zsh modules ---- -----------------------#
 autoload -U promptinit && promptinit
 autoload -U compinit && compinit
--- prompt.zsh
+++ prompt.zsh
@@ -4,17 +4,8 @@
 setopt PROMPT_SUBST               # Do prompt command processing
 
 #---- prompt style - powerlevel10k ---#
-POWERLEVEL9K_MODE="nerdfont-complete"
 source $HOME/.zsh/powerlevel10k/powerlevel10k.zsh-theme
-
-POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
-
-POWERLEVEL9K_VI_MODE_NORMAL_FOREGROUND='red'
-POWERLEVEL9K_VI_MODE_NORMAL_BACKGROUND='white'
-POWERLEVEL9K_VI_COMMAND_MODE_STRING="[CMD]"
-POWERLEVEL9K_VI_MODE_INSERT_FOREGROUND='gray'
-POWERLEVEL9K_VI_MODE_INSERT_BACKGROUND='green'
-POWERLEVEL9K_VI_INSERT_MODE_STRING="[INS]"
+[[ ! -f $HOME/.zsh/powerlevel10k.custom.zsh ]] || source $HOME/.zsh/powerlevel10k.custom.zsh
 
 ################################################################
 # Segment to display pyenv information
@@ -24,7 +15,7 @@ GLOBAL_ICON=$'\UF0AC'
 LOCAL_ICON=$'\UE5FC'
 SHELL_ICON=$'\UF489'
 # set_default POWERLEVEL9K_PYENV_PROMPT_ALWAYS_SHOW false
-prompt_pyenv() {
+prompt_my_pyenv() {
   # First, check if local pyenv is set.
   if [[ -n "$PYENV_VERSION" ]]; then
     p10k segment -b "green" -t "$PYENV_VERSION $SHELL_ICON $PYTHON_ICON"
@@ -53,13 +44,3 @@ prompt_pyenv() {
     fi
   fi
 }
-
-# #### DEFAULT ####
-# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
-# POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
-# #### ####### ####
-
-POWERLEVEL9K_CONTEXT_TEMPLATE="%n@%m" # Default is user@host "%n@%m", we may want to override if we're also using a user segment
-
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode background_jobs os_icon context_joined ssh dir dir_writable vcs newline status root_indicator)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(pyenv time)
