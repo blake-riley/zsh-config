b143b56f8d3e6fe6b73dceb1862647719a9ad677
Blake Riley <briley1@gc.cuny.edu>
2020-02-14 11:09:57 -0500

    Start splitting zshconfig based on OS.

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,24 @@
 #!/usr/bin/env zsh
 
 #------ ls family ----- -----------------------#
-alias ls='ls -hFG'      # add colors for filetype recognition
-alias ll='ls -l'        # show in list
-alias la='ls -Al'       # show hidden files
-alias lx='ls -lXB'      # sort by extension
-alias lk='ls -lSr'      # sort by size, biggest last
-alias lc='ls -ltcr'     # sort by and show change time, most recent last
-alias lu='ls -ltur'     # sort by and show access time, most recent last
-alias lt='ls -ltr'      # sort by date, most recent last
-alias lm='ls -al |more' # pipe through 'more'
-alias lr='ls -lR'       # recursive ls
-alias tree='tree -Csu'  # nice alternative to 'recursive ls'
+case `uname` in
+	Darwin)
+		alias ls='ls -hFG'      	# add colors and */=>@| indicators
+	;;
+	Linux)
+		alias ls='ls -hF --color'	# add colors and */=>@| indicators
+	;;
+esac
+alias ll='ls -l'         # show in list
+alias la='ls -Al'        # show hidden files
+alias lx='ls -lXB'       # sort by extension
+alias lk='ls -lSr'       # sort by size, biggest last
+alias lc='ls -ltcr'      # sort by and show change time, most recent last
+alias lu='ls -ltur'      # sort by and show access time, most recent last
+alias lt='ls -ltr'       # sort by date, most recent last
+alias lm='ls -al | more' # pipe through 'more'
+alias lr='ls -lR'        # recursive ls
+alias tree='tree -Csu'   # nice alternative to 'recursive ls'
 
 #---- pip upgrade ----- -----------------------#
 alias pipup="pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U"
