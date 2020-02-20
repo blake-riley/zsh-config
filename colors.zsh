8a352530150c38e3c720b90c7a492e534f9f44a4
Blake Riley <briley1@gc.cuny.edu>
2020-02-20 11:51:40 -0500

    Comment out color stuff

--- colors.zsh
+++ colors.zsh
@@ -15,10 +15,10 @@ export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
 export PR_BOLD_WHITE PR_BOLD_BLACK
 
 # Clear LSCOLORS
-unset LSCOLORS
+# unset LSCOLORS
 
 # Main change, you can see directories on a dark background
-#expor tLSCOLORS=gxfxcxdxbxegedabagacad
+# export LSCOLORS=gxfxcxdxbxegedabagacad
 
-export CLICOLOR=1
-export LS_COLORS=exfxcxdxbxegedabagacad
+# export CLICOLOR=1
+# export LS_COLORS=exfxcxdxbxegedabagacad
