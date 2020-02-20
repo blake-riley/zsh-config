2f7ea2d6e5afca5bc72bd36e6f452d2c87105ce4
Blake Riley <briley1@gc.cuny.edu>
2020-02-20 11:13:47 -0500

    Only run pyenv and rbenv commands if they exist

--- source_apps.zsh
+++ source_apps.zsh
@@ -2,18 +2,18 @@
 
 #- scripts to source -- -----------------------#
 
+#-- pyenv-virtualenv --------------------------#
+if command -v pyenv 1>/dev/null 2>&1; then eval "$(pyenv init -)"; fi
+if command -v pyenv-virtualenv-init 1>/dev/null 2>&1; then eval "$(pyenv virtualenv-init -)"; fi
+#-- rbenv -------------------------------------#
+if command -v rbenv 1>/dev/null 2>&1; then eval "$(rbenv init -)"; fi
+
 #--- Gromacs
 # source /usr/local/share/gromacs/GMXRC.bash
 #--- CCP4
 # source /Applications/ccp4-7.0/bin/ccp4.setup-sh
 #--- Phenix
 # source /Applications/phenix-1.14-3260/phenix_env.sh
-#-- pyenv-virtualenv --------------------------#
-# source virtualenvwrapper.sh
-eval "$(pyenv init -)"
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-#-- rbenv -------------------------------------#
-eval "$(rbenv init -)"
 
 #--- TeX fix --- ------ -----------------------#
 #eval `/usr/libexec/path_helper -s`
