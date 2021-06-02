d3e5569e25958d538f8119ea33e0b9ab4566bff5
Blake Riley <briley1@gc.cuny.edu>
2021-06-02 18:10:44 -0400

    Update pyenv initialisation

--- source_apps.zsh
+++ source_apps.zsh
@@ -3,7 +3,11 @@
 #- scripts to source -- -----------------------#
 
 #-- pyenv-virtualenv --------------------------#
-if command -v pyenv 1>/dev/null 2>&1; then eval "$(pyenv init -)"; fi
+if command -v pyenv 1>/dev/null 2>&1; then
+	export PYENV_ROOT="$HOME/.pyenv"
+	export PATH="$PYENV_ROOT/bin:$PATH"
+	eval "$(pyenv init --path)";
+fi
 if command -v pyenv-virtualenv-init 1>/dev/null 2>&1; then eval "$(pyenv virtualenv-init -)"; fi
 #-- rbenv -------------------------------------#
 if command -v rbenv 1>/dev/null 2>&1; then eval "$(rbenv init -)"; fi
