c572e838092d6d599b8e236792580f1f26b6cdda
Blake Riley <blake.riley@gmail.com>
2022-04-19 18:50:38 -0400

    Run conda & pyenv side-by-side
    pyenv remains the primary python version manager
    NB: Make sure to run the conda config command at least once,
        this will create an appropriate ~/.condarc

--- source_apps.zsh
+++ source_apps.zsh
@@ -2,6 +2,24 @@
 
 #- scripts to source -- -----------------------#
 
+#-- conda --------------------------#
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+	eval "$__conda_setup"
+else
+	if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+		. "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+	else
+		export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+	fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+# Don't activate the base environment of conda (let pyenv reign)
+# conda config --set auto_activate_base false
+
 #-- pyenv-virtualenv --------------------------#
 if command -v pyenv 1>/dev/null 2>&1; then
 	export PYENV_ROOT="$HOME/.pyenv"
@@ -10,6 +28,7 @@ if command -v pyenv 1>/dev/null 2>&1; then
 	eval "$(pyenv init -)";
 fi
 if command -v pyenv-virtualenv-init 1>/dev/null 2>&1; then eval "$(pyenv virtualenv-init -)"; fi
+
 #-- rbenv -------------------------------------#
 if command -v rbenv 1>/dev/null 2>&1; then eval "$(rbenv init -)"; fi
 
