5aab6bc6bb86a5798acc8f137e433079fae57c1b
Blake Riley <blake-riley@users.noreply.github.com>
2023-02-20 21:41:12 -0800

    Reorder exports/language package managers/custom apps
    exports.zsh has core ENV vars, package manager stuff, and languages
      - languages often include their own package management
    user-apps.zsh contains the remainder of the PATH injections

--- exports.zsh
+++ exports.zsh
@@ -1,44 +1,19 @@
 #!/usr/bin/env zsh
 
-#--- Security
+#------ SECURITY ------ -----------------------#
 export GPG_TTY="$(tty)"
 
-#--- brew
-# export HOMEBREW_BUILD_FROM_SOURCE=1
-hostname=$(hostname | sed 's/.local//g')
-case $hostname in
-	*Tangor*)	unset HOMEBREW_CASK_OPTS ;;
-	*)		export HOMEBREW_CASK_OPTS="--appdir=${HOME}/Applications" ;;
-esac
-#--- Android
-export ANDROID_HOME="/usr/local/opt/android-sdk"
-#--- Amber
-export AMBERHOME="/sw/amber18"
-export AMBER_PREFIX="/sw/amber18"
-#--- Rosetta
-if $(command -v brew 1>/dev/null 2>&1) && $(brew ls --versions rosetta 1>/dev/null); then
-	export ROSETTA3_DB="$(brew --prefix rosetta)/database"
-fi
+#------ COMPILING ----- -----------------------#
+# export ARCHFLAGS='-arch x86_64'
 
-#--- Python
-export PYENV_ROOT="${HOME}/.pyenv"
-export PIPENV_PYTHON="${PYENV_ROOT}/shims/python"
+#------ EDITING ------- -----------------------#
+export EDITOR=vim
 
-#--- Ruby
-if [[ $(uname -s) == Darwin ]] && $(command -v brew 1>/dev/null 2>&1); then
-	export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
-fi
-
-#--- Perl
-#export PERL5LIB="${HOME}/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
-#export PERL_LOCAL_LIB_ROOT="${HOME}/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
-#export PERL_MB_OPT="--install_base \"${HOME}/perl5\""
-#export PERL_MM_OPT="INSTALL_BASE=${HOME}/perl5"
-
-#-------- PATH -------- -----------------------#
+#------ PACKAGE MANAGER -----------------------#
+export PATH="${HOME}/bin:${PATH}"  # personal apps
 export PATH="${HOME}/.iterm2/bin:${PATH}"  # iterm2_shell_integrations
-export PATH="${AMBERHOME}/bin:${PATH}"  # Amber
 
+# Homebrew path
 CPU=$(uname -p)
 if [[ "$CPU" == "arm" ]]; then
 	export PATH="/opt/homebrew/bin:$PATH"
@@ -47,18 +22,70 @@ else
 	export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 fi
 
-export PATH="${HOME}/bin:${PATH}"
-export PATH="${HOME}/.local/bin:${PATH}"
-export PATH="${PYENV_ROOT}/bin:${PATH}"  # pyenv
+# export HOMEBREW_BUILD_FROM_SOURCE=1
+hostname=$(hostname | sed 's/.local//g')
+case $hostname in
+	*Tangor*)	unset HOMEBREW_CASK_OPTS ;;
+	*)		export HOMEBREW_CASK_OPTS="--appdir=${HOME}/Applications" ;;
+esac
+
+#------ LANGUAGES ----- -----------------------#
+##--- Android ---
+export ANDROID_HOME="/usr/local/opt/android-sdk"
+
+##--- Ruby ---
+if [[ $(uname -s) == Darwin ]] && $(command -v brew 1>/dev/null 2>&1); then
+	export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+fi
+if command -v rbenv 1>/dev/null 2>&1; then eval "$(rbenv init -)"; fi
+
+##--- Rust ---
+export PATH="${HOME}/.cargo/bin:${PATH}"
+
+##--- Haskell ---
 #export PATH="${HOME}/.cabal/bin:${PATH}"  # Haskell
-export PATH="${HOME}/.cargo/bin:${PATH}"  # rustup
-export PATH="${HOME}/perl5/bin${PATH+:}${PATH}"  # Perl
-export PATH="${PATH}:$(dirname '/Applications/APBS.app/Contents/MacOS/apbs_term')"
-export PATH="${PATH}:/opt/xds/bin"  # XDS
-export PATH="${PATH}:${HOME}/.arkade/bin"  # arkade (k3s)
 
-#------ COMPILING ----- -----------------------#
-# export ARCHFLAGS='-arch x86_64'
+##--- Perl ---
+#export PERL5LIB="${HOME}/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"
+#export PERL_LOCAL_LIB_ROOT="${HOME}/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"
+#export PERL_MB_OPT="--install_base \"${HOME}/perl5\""
+#export PERL_MM_OPT="INSTALL_BASE=${HOME}/perl5"
+#export PATH="${HOME}/perl5/bin${PATH+:}${PATH}"
 
-#------ EDITING ------- -----------------------#
-export EDITOR=vim
+##--- Python ---
+###-- pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+if command -v pyenv 1>/dev/null 2>&1; then
+	eval "$(pyenv init --path)";
+	eval "$(pyenv init -)";
+fi
+if command -v pyenv-virtualenv-init 1>/dev/null 2>&1; then eval "$(pyenv virtualenv-init -)"; fi
+
+###-- conda
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+	eval "$__conda_setup"
+else
+	if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+		. "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+	else
+		export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+	fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Don't activate the base environment of conda (let pyenv reign)
+# If .condarc already exists, assume the user has set their prefs correctly.
+if [ ! -f "${HOME}/.condarc" ]; then
+	conda config --set auto_activate_base false
+fi
+
+###-- pipenv
+export PIPENV_PYTHON="${PYENV_ROOT}/shims/python"
+
+###-- pipx
+export PATH="${HOME}/.local/bin:${PATH}"  # pipx
---
+++ user_apps.zsh
@@ -0,0 +1,37 @@
+#!/usr/bin/env zsh
+
+#------ APPS ---------- -----------------------#
+##--- fzf Fuzzy finder
+fzf_prefix="$(brew --prefix fzf)"
+if [[ ! "$PATH" == *${fzf_prefix}/bin* ]]; then
+	if [ -d "${fzf_prefix}" ]; then
+		# export PATH="${PATH:+${PATH}:}${fzf_prefix}/bin"
+		[[ $- == *i* ]] && source "${fzf_prefix}/shell/completion.zsh" 2> /dev/null
+		source "${fzf_prefix}/shell/key-bindings.zsh"
+	fi
+fi
+
+##--- APBS
+export PATH="${PATH}:$(dirname '/Applications/APBS.app/Contents/MacOS/apbs_term')"
+##--- Amber
+export AMBERHOME="/sw/amber18"
+export AMBER_PREFIX="/sw/amber18"
+export PATH="${AMBERHOME}/bin:${PATH}"
+##--- Gromacs
+# source /usr/local/share/gromacs/GMXRC.bash
+##--- Rosetta
+if $(command -v brew 1>/dev/null 2>&1) && $(brew ls --versions rosetta 1>/dev/null); then
+	export ROSETTA3_DB="$(brew --prefix rosetta)/database"
+fi
+##--- CCP4
+# source /Applications/ccp4-7.1/bin/ccp4.setup-sh
+##--- Phenix
+# source /Applications/phenix-1.19.2-4158/phenix_env.sh
+##--- XDS
+export PATH="${PATH}:/opt/xds/bin"
+
+##--- Kubernetes (arkade k3s)
+export PATH="${PATH}:${HOME}/.arkade/bin"
+
+##--- TeX fix
+#eval `/usr/libexec/path_helper -s`
